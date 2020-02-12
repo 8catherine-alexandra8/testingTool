a6ad0ad2853bda8e2822512bc14a9799eed58441
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-02-12 08:55:05 -0700

    required in fs module from node standard library and passed in targetPath as an argument to collectFiles.  It will be the folder or absolute path to the folder that I want collectFiles to investigate/collect from

--- runner.js
+++ runner.js
@@ -1,15 +1,18 @@
+//require in fs module from node standard library
+const fs = require('fs');
 //create class to contain all the functions in this file
 //and export it via module.exports
 class Runner {
 	//initialize an object for storing references to each test file that
 	//needs to be executed
 	constructor() {
-        //define a place to store the test file references
-        this.file = [];
-        //define a function for collecting the test files
-        collectFiles() {
-
-        }
-    }
+		//define a place to store the test file references
+		this.file = [];
+	}
+	//define a function for collecting the test files which will run
+	//asynchronously, with an argument of target path which will
+	//reference the folder or absolute path to the folder that we
+	//want this function to investigate/collect from
+	async collectFiles(targetPath) {}
 }
 module.exports = Runner;
