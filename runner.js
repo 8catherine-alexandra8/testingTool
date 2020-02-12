34356da30abfefd251ef19ce627c2298666f97f2
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-02-12 07:23:05 -0700

    added to Runner class place to store test files and started writing method for collecting test files

--- runner.js
+++ runner.js
@@ -3,6 +3,13 @@
 class Runner {
 	//initialize an object for storing references to each test file that
 	//needs to be executed
-	constructor() {}
+	constructor() {
+        //define a place to store the test file references
+        this.file = [];
+        //define a function for collecting the test files
+        collectFiles() {
+
+        }
+    }
 }
 module.exports = Runner;
