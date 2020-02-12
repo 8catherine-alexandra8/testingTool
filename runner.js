9baa35aaac9a3a6c848620e70563785279976685
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-02-12 07:12:20 -0700

    added a contstructor to Runner class, to initialize an object to be used to store references to each test file that will ultimately need to be executed

--- runner.js
+++ runner.js
@@ -1,4 +1,8 @@
 //create class to contain all the functions in this file
 //and export it via module.exports
-class Runner {}
+class Runner {
+	//initialize an object for storing references to each test file that
+	//needs to be executed
+	constructor() {}
+}
 module.exports = Runner;
