c7ffd805e069f339bf1eef7547914ffb7f92058c
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-02-17 16:30:42 -0700

    added assert library to app.test.js file and used it to test whether or not there is a text input

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,3 +1,5 @@
+//require in assert for easier testing syntax
+const assert = require('assert');
 //first test is just making sure there is a text input in the html document
 //render is an async function so need to use async await here
 it('has a text input', async () => {
@@ -7,4 +9,10 @@ it('has a text input', async () => {
 	//html that is inside of there, which can then be inpected/tested. render is
 	//available globally, so no need to require render into this file
 	const dom = await render('index.html');
+	//using syntax from jsdom documentation, accessing just the input field, from
+	//with the dom object
+	const input = dom.window.document.querySelector('input');
+	//use assert syntax to see if input exists.  Assert will automatically
+	//throw an error if the value of input is falsy
+	assert(input);
 });
